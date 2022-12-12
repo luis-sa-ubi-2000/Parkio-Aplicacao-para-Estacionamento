2cd5519493d72b659d26765e78b0b97e7380c718
Bernardoo10 <75854646+Bernardoo10@users.noreply.github.com>
2022-12-12 21:59:55 +0000

    Add files via upload

--- parkio/app/src/main/java/com/ubi/parkio/MainActivity.java
+++ parkio/app/src/main/java/com/ubi/parkio/MainActivity.java
@@ -1,26 +1,53 @@
-package com.ubi.parkio;
+package pt.ubi.di.pdm.forumpark;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+//okokokokok
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
+    NotificationFragment notificationFragment = new NotificationFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btt = (Button) findViewById(R.id.button);
-        btt.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent act = new Intent(view.getContext(), map_page.class);
-                startActivity(act);
+        bottomNavigationView  = findViewById(R.id.bottom_navigation);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+
+        BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.notification);
+        badgeDrawable.setVisible(true);
+        badgeDrawable.setNumber(8);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.notification:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,notificationFragment).commit();
+                        return true;
+                    case R.id.settings:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,settingsFragment).commit();
+                        return true;
+                }
+
+                return false;
             }
         });
+
     }
 }
